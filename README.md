1bafc402c2bde51569a6fd1235a6b4bfd7f42f04
Luis Rivero <luisdavid2014@gmail.com>
2022-04-11 16:14:46 +0200

    cambio a funciones

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # platzi-curso-practico-javascript
+
+...
+
+## Taller #1: figuras geométricas
+
+- Primer paso: definir las fórmulas
+- Segundo: Implementar la formula en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: Intergrar JS con htmil
---
+++ figuras.js
@@ -0,0 +1,58 @@
+//Código del cauadrado
+
+console.group("Cuadrados");
+//const ladoCuadrado = 5;
+//console.log("los lados del cuadrado mide: " + ladoCuadrado + " cm");
+
+function perimetroCuadrado(lado){
+    return lado * 4;
+};
+
+//console.log("El perimetro del cuadrado mide: " + perimetroCuadrado + " cm");
+
+//const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+function areaCuadrado(lado){
+    return lado * lado;
+};
+
+//console.log("El area del cuadrado mide: " + areaCuadrado + " cmº2");
+console.groupEnd();
+
+//Código del triangulo
+
+console.group("Triangulos");
+
+
+function perimetroTriangulo(lado1, lado2, base){
+    return lado1 + lado2 + base;
+}
+
+function areaTriangulo(base, altura){
+    return (base * altura) / 2;
+}
+
+console.groupEnd();
+
+
+//Código del Circulo
+
+console.group("Circulo");
+
+function diametroCirculo(radio){
+    return radio *2;
+}
+
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+
+function areaCirculo(radio){
+    return (radio * radio) * PI;
+}
+
+const PI = Math.PI;
+
+
+console.groupEnd();
